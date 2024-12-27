f11622035876b3ef071b91d1b84d42a6cf8ef145
Aadil x eDen <edenxgt@gmail.com>
2024-12-27 12:56:57 +0530

    Factorial of a number

---
+++ index.js
@@ -0,0 +1,23 @@
+//* ====== Fibonacci Sequence ====== *//
+// const fibonacci = (n) => {
+// 	const fib = [0, 1];
+// 	for (let i = 2; i < n; i++) {
+// 		fib[i] = fib[i - 1] + fib[i - 2];
+// 	}
+// 	return fib;
+// };
+// console.log(fibonacci(7));
+
+//? Big O = O(n)
+
+//* ====== Factorial of a number ====== *//
+// const factorial = (n) => {
+// 	let fac = 1 
+//    for (let i = 2; i <= n; i++) {
+//       fac *= i
+//    }
+//    return fac;
+// };
+// console.log(factorial(4))
+
+//? Big O = O(n)
