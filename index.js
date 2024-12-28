39d50af9f3810d321d5f37da26fb20e27b405920
Aadil x eDen <edenxgt@gmail.com>
2024-12-28 18:58:11 +0530

    Binary Search

--- index.js
+++ index.js
@@ -70,9 +70,58 @@
 //! ================== Recursion ================== !//
 
 //* ====== Recursive Fibonacci Sequence ====== *//
-const recursiveFibonacci = (n) => {
-	if (n < 2) return n;
-	return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
-};
+// const recursiveFibonacci = (n) => {
+// 	if (n < 2) return n;
+// 	return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+// };
 
-console.log(recursiveFibonacci(6))
+// console.log(recursiveFibonacci(6))
+
+//? Big O = O(2^n)
+
+//* ====== Recursive Factorial of a number ====== *//
+// const recursiveFactorial = (n) => {
+// 	if (n === 0) return 1;
+// 	return n * recursiveFactorial(n - 1);
+// };
+
+// console.log(recursiveFactorial(5));
+
+//? Big O = O(n)
+
+//! ====== Search Algorithms ====== !//
+
+//* ====== Linear Search ====== *//
+// const arr = [4, 3, 23, 4, 53, 5, 6];
+
+// const findTargetElement = (n) => {
+// 	const index = arr.findIndex(val => n === val );
+// 	if (!index) return -1;
+// 	return index
+// };
+// console.log(findTargetElement(356));
+
+//? Big O = O(n)
+
+//* ====== Binary Search ====== *//
+// const arr = [-3, -2, 1, 3, 5, 6, 7, 8, 13];
+
+// const findTargetElementUsingBinarySearch = (n) => {
+// 	let leftIndex = 0;
+// 	let rightIndex = arr.length - 1;
+// 	while (leftIndex <= rightIndex) {
+// 		let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+// 		if (n === arr[middleIndex]) return middleIndex;
+// 		if (n < arr[middleIndex]) {
+// 			rightIndex = middleIndex - 1;
+// 		} else {
+// 			leftIndex = middleIndex + 1;
+// 		}
+// 	}
+// 	return -1;
+// };
+
+// console.log(findTargetElementUsingBinarySearch(11))
+
+//? Big O = O(logn)
+
