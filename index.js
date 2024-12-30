cdf66be12b506ca6dfa5c57b4e19a4f532aec0f3
Aadil x eDen <edenxgt@gmail.com>
2024-12-30 17:48:51 +0530

    Append and Prepend Done

--- index.js
+++ index.js
@@ -178,51 +178,66 @@
 //! ================== Linked List ================== !//
 //* ================================================= *//
 
-class Node {
-	constructor(value) {
-		this.value = value;
-		this.next = null;
-	}
-}
+// class Node {
+// 	constructor(value) {
+// 		this.value = value;
+// 		this.next = null;
+// 	}
+// }
 
-class LinkedList {
-	constructor() {
-		this.head = null;
-		this.size = 0;
-	}
-	isEmpty() {
-		return this.size === 0;
-	}
-	getSize() {
-		return this.size;
-	}
-	prepend(value) {
-		const node = new Node(value);
-		if (!this.isEmpty()) node.next = this.head;
-		this.head = node;
-		this.size++;
-	}
-	print() {
-		if (this.isEmpty()) {
-			console.log("List is Empty");
-		} else {
-			let curr = this.head;
-			let listValues = "";
-			while (curr) {
-				listValues += `${curr.value} `;
-				curr = curr.next;
-			}
-			console.log(listValues);
-		}
-	}
-}
+// class LinkedList {
+// 	constructor() {
+// 		this.head = null;
+// 		this.size = 0;
+// 	}
+// 	isEmpty() {
+// 		return this.size === 0;
+// 	}
+// 	getSize() {
+// 		return this.size;
+// 	}
+// 	//? Big O of prepend = O(1)
+// 	prepend(value) {
+// 		const node = new Node(value);
+// 		if (!this.isEmpty()) node.next = this.head;
+// 		this.head = node;
+// 		this.size++;
+// 	}
 
-const list = new LinkedList();
-console.log(list.isEmpty());
-console.log(list.getSize());
-list.print()
-list.prepend(10);
-list.print()
-list.prepend(20);
-list.prepend(30);
-list.print()
+// 	//? Big O of append = O(n)
+// 	append(value) {
+// 		const node = new Node(value);
+// 		if (this.isEmpty()) {
+// 			this.head = node;
+// 		} else {
+// 			let prev = this.head;
+// 			while (prev.next) {
+// 				prev = prev.next;
+// 			}
+// 			prev.next = node;
+// 		}
+// 		this.size++;
+// 	}
+// 	print() {
+// 		if (this.isEmpty()) {
+// 			console.log("List is Empty");
+// 		} else {
+// 			let curr = this.head;
+// 			let listValues = "";
+// 			while (curr) {
+// 				listValues += `${curr.value} `;
+// 				curr = curr.next;
+// 			}
+// 			console.log(listValues);
+// 		}
+// 	}
+// }
+
+// const list = new LinkedList();
+// list.prepend(10);
+// list.prepend(20);
+// list.prepend(30);
+// list.print();
+// list.append(40);
+// list.append(50);
+// list.print();
